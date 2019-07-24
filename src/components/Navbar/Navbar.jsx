da4cecde98a0f234f7f3c8f55509ace3bd12a367
Alexander Romanovsky <bahentos@gmail.com>
2019-07-24 13:59:00 +0300

    Lesson 15: Folder Structure

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import s from "./Navbar.module.css";
 
 const Navbar = () => {
     return <nav className={s.nav}>
-    <div ><a className={s.item} href="">Profile</a></div>
+    <div ><a className={`{s.item} {s.active}`} href="">Profile</a></div>
     <div ><a className={s.item} href="">Messages</a></div>
     <div ><a className={s.item} href="">News</a></div>
     <div ><a className={s.item} href="">Music</a></div>
