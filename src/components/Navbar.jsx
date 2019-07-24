da5c017390da48caea415dce61da5a30cac8246d
Alexander Romanovsky <bahentos@gmail.com>
2019-07-24 10:20:24 +0300

    Lesson 14: CSS Module

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import s from "./Navbar.module.css";
 
 const Navbar = () => {
-    return <nav className="nav">
-    <div><a href="">Profile</a></div>
-    <div><a href="">Messages</a></div>
-    <div><a href="">News</a></div>
-    <div><a href="">Music</a></div>
-    <div><a href="">Settings</a></div>
+    return <nav className={s.nav}>
+    <div ><a className={s.item} href="">Profile</a></div>
+    <div ><a className={s.item} href="">Messages</a></div>
+    <div ><a className={s.item} href="">News</a></div>
+    <div ><a className={s.item} href="">Music</a></div>
+    <div ><a className={s.item} href="">Settings</a></div>
   </nav>
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import s from "./Profile.module.css";
 
 const Profile = () => {
-    return <div className="main">
+    return <div className={s.main}>
       <img src="https://pegas-kolomna.ru/images/%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8%20%D0%B4%D0%BB%D1%8F%20%D1%81%D1%82%D1%80%D0%B0%D0%BD/ispania.jpg" alt=""/>
       <div>
         Ava+description
@@ -10,8 +10,8 @@ const Profile = () => {
       <div>
         My posts
         <div>New post</div>
-        <div>Post 1</div>
-        <div>Post 2</div>
+        <div className={s.item}>Post 1</div>
+        <div className={s.item}>Post 2</div>
       </div>
   </div>
 }
