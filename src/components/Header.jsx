80ed57aa6d0fe1cf89f666b7f6a2cc9d254f07ce
Alexander Romanovsky <bahentos@gmail.com>
2019-07-24 09:00:19 +0300

    Lesson 11: divide html to components

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Header = () => {
+    return <header className="header">
+    <img src="https://www.weddingprofessionalsofcolumbus.com/wp-content/uploads/logo-ex-7.png" alt=""/>
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return <div className="main">
+      <img src="https://pegas-kolomna.ru/images/%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8%20%D0%B4%D0%BB%D1%8F%20%D1%81%D1%82%D1%80%D0%B0%D0%BD/ispania.jpg" alt=""/>
+      <div>
+        Ava+description
+      </div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+  </div>
+}
+
+export default Profile;
