838edd91ca411324722da87630357af0a0ae8fba
Alexander Romanovsky <bahentos@gmail.com>
2019-07-31 10:46:24 +0300

    Make design from http://preview-vuse.hexesis.com/app/contacts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import s from "./Header.module.css";
+import avatarimg from "./img/avatar.jpg";
 
 const Header = () => {
   return (
     <header className={s.header}>
-      <img
-        src="https://www.weddingprofessionalsofcolumbus.com/wp-content/uploads/logo-ex-7.png"
-        alt=""
-      />
+      <div className={s.arrow}>
+      </div>
+      <div className={s.avatarCont}>
+        <span>Tom Waits</span>
+        <img src={avatarimg} className={s.avatar}/>
+      </div>
     </header>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,44 @@
 import React from "react";
 import s from "./Navbar.module.css";
-import { NavLink} from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
-      <div>
-        <NavLink activeClassName={s.active} className={s.item} to="/profile">
+      <div className={s.logo}>
+        <img
+          src="http://preview-vuse.hexesis.com/static/vuse-circle-white.svg"
+          alt=""
+        />
+        <span className={s.name}>Соц-сеть</span>
+      </div>
+      <div className={s.cont}>
+        <NavLink activeClassName={s.active} className={s.item + " " + s.profile} to="/profile">
+          <div className={s.pict} />
           Profile
         </NavLink>
       </div>
-      <div>
-        <NavLink activeClassName={s.active} className={s.item} to="/dialogs">
+      <div className={s.cont}>
+        <NavLink activeClassName={s.active} className={s.item + " " + s.messages} to="/dialogs">
+          <div className={s.pict} />
           Messages
         </NavLink>
       </div>
-      <div>
-        <NavLink activeClassName={s.active} className={s.item} to="/news">
+      <div className={s.cont}>
+        <NavLink activeClassName={s.active} className={s.item + " " + s.news} to="/news">
+          <div className={s.pict} />
           News
         </NavLink>
       </div>
-      <div>
-        <NavLink activeClassName={s.active} className={s.item} to="/music">
+      <div className={s.cont}>
+        <NavLink activeClassName={s.active} className={s.item + " " + s.music} to="/music">
+        <div className={s.pict} />
           Music
         </NavLink>
       </div>
-      <div>
-        <NavLink activeClassName={s.active} className={s.item} to="/settings">
+      <div className={s.cont}>
+        <NavLink activeClassName={s.active} className={s.item + " " + s.settings} to="/settings">
+        <div className={s.pict} />
           Settings
         </NavLink>
       </div>
