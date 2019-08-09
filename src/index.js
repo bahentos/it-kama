8ff7cbc3a528cb73925455387bdc40b5901ff9bb
Alexander Romanovsky <bahentos@gmail.com>
2019-08-09 13:51:15 +0300

    Lesson-37: рефакторинг

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import state from './redux/state';
-import { rerenderEntireTree } from './render';
+import store from './redux/state';
+import './index.css';
+import App from './App';
 
 
-rerenderEntireTree(state);
 
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(<App state={state} addPost={store.addPost.bind(store)} updatePostText={store.updatePostText.bind(store)} />, document.getElementById('root'));
+}
+
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,8 +1,7 @@
-import { rerenderEntireTree } from '../render';
-
-const state = {
-  dialogsPage: {
-    dialogsData: [
+let store = {
+  _state: {
+    dialogsPage: {
+      dialogsData: [
         { id: 1, name: "Tomas Mann", avatar: "img/avatar-1.png" },
         { id: 2, name: "Ernst Ham", avatar: "img/avatar-2.png" },
         { id: 3, name: "Mister Dool", avatar: "img/avatar-3.png" },
@@ -14,46 +13,56 @@ const state = {
           name: "Richard Crosby",
           avatar: "img/avatar-6.png"
         }
-    ],
+      ],
 
-    dialogsMessage: [
-      { id: 1, message: "Truth is more of a stranger than fiction" },
-      { id: 2, message: "When you want it, it goes away too fast" },
-      { id: 3, message: "Live without regrets" },
-      { id: 4, message: "Follow your heart" },
-      { id: 5, message: "Lost time is never found again" },
-      { id: 6, message: "Respect the past, create the future!" }
-    ]
+      dialogsMessage: [
+        { id: 1, message: "Truth is more of a stranger than fiction" },
+        { id: 2, message: "When you want it, it goes away too fast" },
+        { id: 3, message: "Live without regrets" },
+        { id: 4, message: "Follow your heart" },
+        { id: 5, message: "Lost time is never found again" },
+        { id: 6, message: "Respect the past, create the future!" }
+      ]
+    },
+
+    mypostPage: {
+      messagesData: [
+        {
+          id: 1,
+          likeCount: 12,
+          message: `Pedestrians running for shelter into the market and under the portico
+        of St. Paul's Church, where there are already several people, among them alady and her daughter in evening dress.
+        They are all peering out gloomily at the rain, except one man with hisback turned to the rest, who seems wholly preoccupied with a notebookin which he is writing busily.
+        `
+        },
+        { id: 2, likeCount: 24, message: "Everyone has one's own path" }
+      ],
+      newPostText: ""
+    }
+  },
+  _callSubscriber() {},
+  addPost(postMessage) {
+    let newPost = {
+      id: 3,
+      message: this._state.mypostPage.newPostText,
+      likeCount: 0
+    };
+    this._state.mypostPage.messagesData.push(newPost);
+    this._state.mypostPage.newPostText = "";
+    this._callSubscriber(this._state);
+  },
+  updatePostText(newText) {
+    this._state.mypostPage.newPostText = newText;
+    this._callSubscriber(this._state);
   },
 
-  mypostPage: {
-    messagesData: [
-      { id: 1, likeCount: 12, message: `Pedestrians running for shelter into the market and under the portico
-      of St. Paul's Church, where there are already several people, among them alady and her daughter in evening dress.
-      They are all peering out gloomily at the rain, except one man with hisback turned to the rest, who seems wholly preoccupied with a notebookin which he is writing busily.
-      ` },
-      { id: 2, likeCount: 24, message: "Everyone has one's own path" },
-    ],
-    newPostText: "",
-  }
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
+  getState() {
+    return this._state;
+  },
 };
 
-window.state = state;
-
-export let addPost = (postMessage) => {
-  let newPost = {
-    id: 3,
-    message: state.mypostPage.newPostText,
-    likeCount: 0
-  };
-  state.mypostPage.messagesData.push(newPost);
-  rerenderEntireTree(state);
-  state.mypostPage.newPostText = "";
-}
-
-export let updatePostText = (newText) => {
-  state.mypostPage.newPostText = newText;
-  rerenderEntireTree(state);
-}
-
-export default state;
+window.state = store;
+export default store;
