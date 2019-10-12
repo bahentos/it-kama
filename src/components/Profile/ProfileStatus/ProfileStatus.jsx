cc8299165f7347f77d59597d51b82ad0521fd7a0
Alexander Romanovsky <bahentos@gmail.com>
2019-10-12 22:58:17 +0300

    Lesson 74: componentDidUpdate

--- src/components/Profile/ProfileStatus/ProfileStatus.jsx
+++ src/components/Profile/ProfileStatus/ProfileStatus.jsx
@@ -6,7 +6,13 @@ class ProfileStatus extends React.Component {
     state = {
         editMode: false,
         status: this.props.status,
-    };
+};
+
+   componentDidUpdate(prevProps, prevState) {
+       if (prevProps.status !== this.props.status) {
+           this.setState({status: this.props.status})
+       }
+   }
 
     activateEditMode = () => {
         this.setState({editMode: true})
